69d97b845a6afcc40229f2e248aa9cabc08fea36
Magdalena-Ramirez <Magirez00@gmail.com>
2021-05-26 18:00:55 -0700

    Add files via upload
    complete basic functioning calculator

---
+++ learn.js
@@ -0,0 +1,22 @@
+function adder(n1, n2){
+    //let sum = n1 + n2
+    return n1 + n2
+}
+
+function subtract(n1, n2){
+    //let sub = n1 - n2
+    return n1 - n2
+}
+
+function multipy(n1, n2){
+    //let mul = n1 * n2
+    return n1 * n2
+}
+
+function division(n1, n2){
+    return n1 / n2
+}
+
+let sum = adder(3,3)
+
+console.log(sum)
